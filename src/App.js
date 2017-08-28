f6041484bedfb8d926f471db7a77dad43ed8c1b9
Eric Kramer <theerickramer@gmail.com>
2017-08-28 15:13:01 -0400

    nav main with flex

--- src/App.js
+++ src/App.js
@@ -1,31 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Styleguide</h2>
-        </div>
-        <p className="App-intro">
-          Welcome to Styleguide
-        </p>
+        <h1>
+          Styleguide
+        </h1>
         <nav>
           <ul>
             <li>color</li>
           </ul>
         </nav>
-        <div>
+        <main>
           <div className="color purple"></div>
           <div className="color pink"></div>
           <div className="color green"></div>
           <div className="color yellow"></div>
           <div className="color blue"></div>
           <div className="color orange"></div>
-        </div>
+        </main>
       </div>
     );
   }
