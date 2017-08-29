f2fc5ad35509be82d24fdba40dad4dc0a1132829
Eric Kramer <theerickramer@gmail.com>
2017-08-29 14:55:01 -0400

    typography component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
 import { Link, Route } from 'react-router-dom';
 import Color from './color/Color';
+import Typography from './typography/Typography';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Styleguide</h1>
-        <nav>
-          <ul>
-            <li><Link to="/color">color</Link></li>
-          </ul>
-        </nav>
-        <main>
-          <Route path="/color" component={Color} />
-        </main>
+        <div className="App__head">
+          <h1>Styleguide</h1>
+        </div>
+        <div className="App__body">
+          <nav>
+            <ul>
+              <li>
+                <Link to="/color">Color</Link>
+              </li>
+              <li>
+                <Link to="/typography">Typography</Link>
+              </li>
+            </ul>
+          </nav>
+          <main>
+            <Route path="/color" component={Color} />
+            <Route path="/typography" component={Typography} />
+          </main>
+        </div>
       </div>
     );
   }
