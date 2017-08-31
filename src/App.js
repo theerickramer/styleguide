0cc2ff54a974b89c32f323cb57df85ffbfb33c17
Eric Kramer <theerickramer@gmail.com>
2017-08-30 20:06:23 -0400

    moved Router to App render test passes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Color from './color/Color';
 import Typography from './typography/Typography';
 import './App.css';
@@ -7,27 +7,29 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App__head">
-          <h1>Styleguide</h1>
+      <Router>
+        <div className="App">
+          <div className="App__head">
+            <h1>Styleguide</h1>
+          </div>
+          <div className="App__body">
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/color">Color</Link>
+                </li>
+                <li>
+                  <Link to="/typography">Typography</Link>
+                </li>
+              </ul>
+            </nav>
+            <main>
+              <Route path="/color" component={Color} />
+              <Route path="/typography" component={Typography} />
+            </main>
+          </div>
         </div>
-        <div className="App__body">
-          <nav>
-            <ul>
-              <li>
-                <Link to="/color">Color</Link>
-              </li>
-              <li>
-                <Link to="/typography">Typography</Link>
-              </li>
-            </ul>
-          </nav>
-          <main>
-            <Route path="/color" component={Color} />
-            <Route path="/typography" component={Typography} />
-          </main>
-        </div>
-      </div>
+      </Router>
     );
   }
 }
