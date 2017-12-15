cd81a9b86cbb287a1358a18f63251900634bb66a
Eric Kramer <theerickramer@gmail.com>
2017-12-15 11:13:36 -0500

    typpography font & size

--- src/typography/Typography.js
+++ src/typography/Typography.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 
 export default class Typography extends Component {
-    render() {
-        return( <div>
-            <h1>h1</h1>
-            <h2>h2</h2>
-            <h3>h3</h3>
-            <h4>h4</h4>
-            <h5>h5</h5>
-            <h6>h6</h6>
-        </div>)
-    }
+  render() {
+    return (
+      <div>
+        <h1>h1</h1>
+        <h2>h2</h2>
+        <h3>h3</h3>
+        <h4>h4</h4>
+        <h5>h5</h5>
+        <h6>h6</h6>
+        <p>p</p>
+      </div>
+    );
+  }
 }
